38a100cff10a113bba05f1bded1d93737d88ab8c
Mahesh palaka <20A91A04P1@aec.edu.in>
2021-07-09 20:55:42 +0530

    Add files via upload
    print Last digit of pow(n,k)

---
+++ MBA_Challenge.java
@@ -0,0 +1,15 @@
+package packcodecamp;
+import java.util.*;
+public class MBA_Challenge {
+
+	public static void main(String[] args) {
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Input1:");
+			int n = sc.nextInt(); // 3
+			System.out.println("Input2:");
+			int k = sc.nextInt(); 
+			int s = (int)Math.pow(n, k);
+			int rem = s%10; 
+	        System.out.println(rem);
+		}
+	}
